06198ae5fcb55b9ce13c77fe379732c21ffafeb8
Majid5654 <3rwanmajid@gmail.com>
2025-05-20 10:58:17 +0700

    first

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,31 +18,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'role_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    public function role(){
+        return $this->belongsTo(Role::class);
     }
+   
 }
