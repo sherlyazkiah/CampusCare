8f033ddc179d790ad4fb22616d9e909318f9ad1d
Majid5654 <3rwanmajid@gmail.com>
2025-05-25 19:19:56 +0700

    fix login

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,32 @@ use App\Http\Controllers\FacilityController;
 use App\Http\Controllers\FloorController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;   
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 //Admin
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {return view('admin.dashboard');
-});
-    Route::get('/userdata', [UserController::class, 'view'])->name('userdata.index');
-     Route::get('/userdata/create', [UserController::class, 'create'])->name('userdata.create');
-     Route::post('/userdata', [UserController::class, 'store'])->name('userdata.store');
+Route::middleware(['auth', 'authorize:admin'])->prefix('admin')->group(function () {
 
-    Route::get('/floorroomdata', [FloorController::class, 'view'])->name('floorroomdata.index');
-    Route::get('/facilitydata', [FacilityController::class, 'view'])->name('facilitydata.index');
-     Route::get('/userdata/{id}/edit', [UserController::class, 'edit'])->name('userdata.edit');
-     Route::put('/userdata/{id}', [UserController::class, 'update'])->name('userdata.update');
-});
+    // Dashboard Admin
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    // User Data
+    Route::get('/userdata', [UserController::class, 'view'])->name('userdata.index');
+    Route::get('/userdata/create', [UserController::class, 'create'])->name('userdata.create');
+    Route::post('/userdata', [UserController::class, 'store'])->name('userdata.store');
+    Route::get('/userdata/{id}/edit', [UserController::class, 'edit'])->name('userdata.edit');
+    Route::put('/userdata/{id}', [UserController::class, 'update'])->name('userdata.update');
+    });
+
+//User
 
